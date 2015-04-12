767dad07d73ba12432ceda16ec5f650b6eaee7b5
Bart Hoffman <info@barthoffman.nl>
2015-04-12 17:16:54 +0200

    initial Commit

---
+++ nl/Voorbeelden_werk/Paginas/Deuren_files/Deuren.js
@@ -0,0 +1,29 @@
+// Created by iWeb 3.0.4 local-build-20121113
+
+function createMediaStream_id1()
+{return IWCreatePhotocast("http://www.janhartjes.nl/nl/Voorbeelden_werk/Paginas/Deuren_files/rss.xml",true);}
+function initializeMediaStream_id1()
+{createMediaStream_id1().load('http://www.janhartjes.nl/nl/Voorbeelden_werk/Paginas',function(imageStream)
+{var entryCount=imageStream.length;var headerView=widgets['widget1'];headerView.setPreferenceForKey(imageStream.length,'entryCount');NotificationCenter.postNotification(new IWNotification('SetPage','id1',{pageIndex:0}));});}
+function layoutMediaGrid_id1(range)
+{createMediaStream_id1().load('http://www.janhartjes.nl/nl/Voorbeelden_werk/Paginas',function(imageStream)
+{if(range==null)
+{range=new IWRange(0,imageStream.length);}
+IWLayoutPhotoGrid('id1',new IWPhotoGridLayout(3,new IWSize(182,182),new IWSize(182,35),new IWSize(218,232),27,27,0,new IWSize(18,18)),new IWPhotoFrame([IWCreateImage('Deuren_files/Hardcover_bevel_01.png'),IWCreateImage('Deuren_files/Hardcover_bevel_02.png'),IWCreateImage('Deuren_files/Hardcover_bevel_03.png'),IWCreateImage('Deuren_files/Hardcover_bevel_06.png'),IWCreateImage('Deuren_files/Hardcover_bevel_09.png'),IWCreateImage('Deuren_files/Hardcover_bevel_08.png'),IWCreateImage('Deuren_files/Hardcover_bevel_07.png'),IWCreateImage('Deuren_files/Hardcover_bevel_04.png')],null,0,0.500000,0.000000,0.000000,0.000000,0.000000,17.000000,17.000000,17.000000,17.000000,403.000000,295.000000,403.000000,295.000000,null,null,null,0.100000),imageStream,range,null,null,1.000000,{backgroundColor:'rgb(0, 0, 0)',reflectionHeight:100,reflectionOffset:2,captionHeight:100,fullScreen:0,transitionIndex:2},'../../Media/slideshow.html','widget1','widget2','widget3')});}
+function relayoutMediaGrid_id1(notification)
+{var userInfo=notification.userInfo();var range=userInfo['range'];layoutMediaGrid_id1(range);}
+function onStubPage()
+{var args=window.location.href.toQueryParams();parent.IWMediaStreamPhotoPageSetMediaStream(createMediaStream_id1(),args.id);}
+if(window.stubPage)
+{onStubPage();}
+setTransparentGifURL('../../Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{IWRegisterNamedImage('comment overlay','../../Media/Photo-Overlay-Comment.png')
+IWRegisterNamedImage('movie overlay','../../Media/Photo-Overlay-Movie.png')
+loadMozillaCSS('Deuren_files/DeurenMoz.css')
+NotificationCenter.addObserver(null,relayoutMediaGrid_id1,'RangeChanged','id1')
+Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('../../Media/transparent.gif');fixupIECSS3Opacity('id2');initializeMediaStream_id1()
+performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
---
+++ nl/Welkom_files/Welkom.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20121113
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Welkom_files/WelkomMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
